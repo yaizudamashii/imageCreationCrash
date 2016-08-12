ab6d2d5fce68d47920a613f596ab2556ba58c146
Yuki Konda <yuki.konda@gmail.com>
2016-08-12 17:55:25 -0400

    Initial Commit

---
+++ imageCreationCrash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageCreationCrash
+//
+//  Created by Yuki Konda on 8/12/16.
+//  Copyright © 2016 Yuki Konda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
